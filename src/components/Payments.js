6c3f3561c0f8d1ba899546cd2d1845cbaf935c83
WilliamG <williamalberto1@hotmail.com>
2022-09-06 21:13:20 -0500

    Table added, pending hidratation error

--- src/components/Payments.js
+++ src/components/Payments.js
@@ -1,16 +1,32 @@
 import React from "react";
 
 const Payments = ({ state }) => {
-  const { arrPayments } = state;
-  console.log("Paso 1")
-  console.log(arrPayments)
-  return (
-    <div className="tip-result">
-      <div>
-        <p className="label">Total Tip</p>
-      </div>
+    const { arrPayments } = state;
+    console.log("Paso 1");
+    console.log(arrPayments);
+    return (
+    <div className="payment-list">
+        <table className="payments-table">
+            <tr>
+                <th>Payment</th>
+                <th>Initial Balance</th>
+                <th>Interest</th>
+                <th>Amortization</th>
+                <th>Final Balance</th>
+            </tr>
+            {arrPayments.map((p) => {
+                return (
+                    <tr key={p.numberPayment}>
+                        <td>{`${p.numberPayment}`}</td>
+                        <td>{`${p.intialBalance}`}</td>
+                        <td>{`${p.interestValue}`}</td>
+                        <td>{`${p.amortization}`}</td>
+                        <td>{`${p.finalBalance}`}</td>
+                    </tr>
+            )})}
+        </table>
     </div>
-  );
+    );
 };
 
 export default Payments;
